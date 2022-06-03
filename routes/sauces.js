fa59e02463cfd4e1eace2589202dd61adff431e7
Sofiane Idir <idir.sofiane@yahoo.com>
2022-06-03 20:02:52 +0200

    authentification + token

--- routes/sauces.js
+++ routes/sauces.js
@@ -1,13 +1,14 @@
 const express = require('express');
 
 const router = express.Router();
-const saucesCtrl = require('../controllers/sauces')
+const saucesCtrl = require('../controllers/sauces');
+const auth = require('../middleware/auth');
 
 // Routes avec les métiers 
-router.post('/', saucesCtrl.createSauces);
-router.put('/:id', saucesCtrl.modifySauces);
-router.delete('/:id', saucesCtrl.deleteSauces)
-router.get('/:id', saucesCtrl.getOneSauce);
-router.get('/', saucesCtrl.getAllSauces);
+router.post('/', auth, saucesCtrl.createSauces);
+router.put('/:id', auth, saucesCtrl.modifySauces);
+router.delete('/:id', auth, saucesCtrl.deleteSauces)
+router.get('/:id', auth, saucesCtrl.getOneSauce);
+router.get('/', auth, saucesCtrl.getAllSauces);
 
 module.exports = router;
